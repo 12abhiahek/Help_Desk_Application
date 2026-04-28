b61a5156f69d6c3f75a5a5b858d867f2409ef11d
12abhiahek <abhisheks141999@gmail.com>
2026-04-28 23:32:08 +0530

    initial_setup _for_helpDeskApplication

---
+++ src/main/java/com/helpdesk/HelpDeskWebApplication/HelpDeskWebApplication.java
@@ -0,0 +1,13 @@
+package com.helpdesk.HelpDeskWebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpDeskWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpDeskWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helpdesk/HelpDeskWebApplication/HelpDeskWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpdesk.HelpDeskWebApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
